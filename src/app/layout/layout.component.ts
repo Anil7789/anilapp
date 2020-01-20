8ac8a4d8d06d749bb68cfc34c1f98e0bfb55b1d0
anilreddy0536@gmail.com <anil.k@thresholdsoft.com>
2020-01-20 17:28:09 +0530

    added some content

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -7,6 +7,7 @@ import { EmpService } from '../emp.service';
   styleUrls: ['./layout.component.scss']
 })
 export class LayoutComponent implements OnInit {
+  header = 'Layout page';
   todaydate;
   constructor(private myService: EmpService) { }
 
